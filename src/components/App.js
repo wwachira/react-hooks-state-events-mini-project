e1e8a92988ff3c0919673c7521341f93f704362b
lizwachira <lizwanguwachira@gmail.com>
2024-05-13 13:41:28 +0300

    feat; tests pass

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTask] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+
+  function handleDeleteTask(deletedTask) {
+    const newTaskList = tasks.filter((task) => {
+      return task.text !== deletedTask;
+    });
+    setTask(newTaskList);
+  }
+
+  function handleAddTask(newTask) {
+    if (newTask.text.trim() !== "" && newTask.category !== "All") {
+      setTask([...tasks, newTask]);
+    }
+  }
+
+  const filteredTasks = tasks.filter((task) => {
+    if (category === "All") {
+      return true;
+    } else {
+      return task.category === category;
+    }
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        Scategory={category}
+        onCategoryClick={setCategory}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onDeleteTask }) {
+  const displayTask = tasks.map((task) => {
+    return (
+      <Task
+        key={task.text}
+        text={task.text}
+        category={task.category}
+        onDeleteTask={onDeleteTask}
+      />
+    );
+  });
+
+  return <div className="tasks">{displayTask}</div>;
 }
 
 export default TaskList;
