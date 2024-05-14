1ace84f5d2be237155ee74ad4ebc8f0f961f0b19
lizwachira <lizwanguwachira@gmail.com>
2024-05-14 12:39:33 +0300

    comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+//{useState} hook init `tasks` state
 import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
@@ -7,6 +8,7 @@ import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
+//event handlers are 2: 1)`handleDeleteTask` deletes a task from the list, filters out and updates state.
 function App() {
   const [tasks, setTask] = useState(TASKS);
   const [category, setCategory] = useState("All");
@@ -18,15 +20,17 @@ function App() {
     setTask(newTaskList);
   }
 
+  // 2)`handleAddTask`  adds new task, and updates.
   function handleAddTask(newTask) {
     if (newTask.text.trim() !== "" && newTask.category !== "All") {
       setTask([...tasks, newTask]);
     }
   }
-
+//`filteredTasks` selectscategory, all tasks display of the particular category.
   const filteredTasks = tasks.filter((task) => {
     if (category === "All") {
       return true;
+
     } else {
       return task.category === category;
     }
